f04be56a51cd89bf010e10ee6796786d416ebad0
Mythri P K <mythripk@ti.com>
2011-02-14 15:48:37 +0100

    OMAP : DSS : HDMI: HDMI specific display controller and dss change.
    Adding changes to set gamma table bit for TV interface and function to select
    between VENC and HDMI.
    
    Signed-off-by: Mythri P K <mythripk@ti.com>

--- drivers/video/omap2/dss/dss.c
+++ drivers/video/omap2/dss/dss.c
@@ -559,6 +559,13 @@ void dss_set_dac_pwrdn_bgz(bool enable)
 	REG_FLD_MOD(DSS_CONTROL, enable, 5, 5);	/* DAC Power-Down Control */
 }
 
+void dss_select_hdmi_venc(bool hdmi)
+{
+	REG_FLD_MOD(DSS_CONTROL, hdmi, 15, 15);	/* 0x1 for HDMI, 0x0 VENC */
+	if (hdmi)
+		REG_FLD_MOD(DSS_CONTROL, 0, 9, 8);
+}
+
 static int dss_init(bool skip_init)
 {
 	int r;
